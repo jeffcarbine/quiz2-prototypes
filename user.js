b820d656e3095d8e91d4e561bad1e2f50a2b025c
jeffcarbine <jeffcarbine@gmail.com>
2016-02-02 09:22:17 -0700

    Did the assignment in-class
    Done.

--- user.js
+++ user.js
@@ -1,7 +1,44 @@
 'use strict';
 
-function User(username, password) {
+var bcrypt = require('bcrypt');
+var assert = require('assert');
 
+var users = [];
+
+function User(username, password) {
+  var userExists = User.find(username);
+
+  assert(!userExists, 'That username is taken.');
+
+  this.username = username;
+  var salt = bcrypt.genSaltSync(10);
+  this.password = bcrypt.hashSync(password, salt);
+
+  users.push(this);
 }
+
+User.prototype.authenticate = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+User.find = function(username) {
+  var foundUser = users.find(function(user) {
+    return user.username === username;
+  });
+
+  return foundUser || null;
+};
+
+User.authenticate = function(username, password) {
+  var user = User.find(username);
+
+  if (!user) { return false; }
+
+  var validPassword = user.authenticate(password);
+
+  if (!validPassword) { return false; }
+
+  return user;
+};
 
 module.exports = User;
